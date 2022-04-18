9a2242aabe0d719d3c7d1978792d3927b27ca148
gdavid04 <gabordavid04@gmail.com>
2022-04-18 23:42:30 +0200

    Client only

--- src/main/java/gdavid/psionicutilities/PsionicUtilities.java
+++ src/main/java/gdavid/psionicutilities/PsionicUtilities.java
@@ -1,10 +1,20 @@
 package gdavid.psionicutilities;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(PsionicUtilities.modId)
 public class PsionicUtilities {
 	
 	public static final String modId = "psionicutilities";
+	
+	public PsionicUtilities() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+			() -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
 	
 }
